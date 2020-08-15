405d63908529ec6e287c00ce2ba640ed01ea5c7c
Chinthan Rk <47713099+Chinthan-Rk@users.noreply.github.com>
2020-08-15 19:45:57 +0530

    Initial commit

---
+++ androidStudioProjects/RazorPay/app/src/main/java/com/example/razorpay/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.razorpay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
